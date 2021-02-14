64bb8b055beaeb8d6e94b47b12e2fa6f4b4daf6d
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2021-02-14 22:03:08 +0600

    organized

---
+++ LAB -1/MRRSirLabExam1Work/Lab 20210123/submissions/12_A.cpp
@@ -0,0 +1,9 @@
+//12
+//12
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello.\nThis is a test program.\nI passed this test with 100%s marks.\n","%");
+	return 0;
+}
---
+++ LAB-3/Lab 3 20210207/submissions/12_A.cpp
@@ -0,0 +1,17 @@
+//12
+//12
+//12
+//12
+ #include<stdio.h>
+int main()
+{
+    char str[25];
+    fgets(str,25,stdin);
+    if(str[0]=='2' && str[1]=='1')
+    {
+        printf("International Mother Language Day!\n");
+    }
+    else
+        printf("I don't care!\n");
+    return 0;
+} 
