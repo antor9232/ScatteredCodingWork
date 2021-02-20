7922d1b56b62b0bbf790bd11ce132a4366466829
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2021-02-20 16:51:56 +0600

    m

--- incourse-assignment/12_E.cpp
+++ incourse-assignment/12_E.cpp
@@ -1,32 +1,40 @@
 #include <stdio.h>
+
 typedef long long ll;
+int len = 0;
 
 typedef struct String4
 {
     char s[5];
 } String4;
-struct String4 sarray[100000];
-void strcpy(char *a, char *b){
-    int i=0;
-    while(1){
-         char temp= a[i];
-        a[i]= b[i];
-        b[i]= temp;
-        if(a[i]=='\0') break; 
+struct String4 sarray[1000000];
+
+int strcmp(char *a, char *b)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (a[i] < b[i])
+            return -1;
+        else if (a[i] > b[i])
+            return 1;
+    }
+    return 0;
+}
+void strcpy(char *a, char *b)
+{
+    int i = 0;
+    while (1)
+    {
+        char temp = a[i];
+        a[i] = b[i];
+        b[i] = temp;
+        if (a[i] == '\0')
+            break;
         i++;
     }
     return;
 }
 
-void swap(struct String4 a, struct String4 b)
-{
-    printf("Ran swap\n");
-    struct String4 temp;
-    temp = a;
-    a = b;
-    b = temp;
-    return;
-}
 int strlen(char *s)
 {
     int i = 0, count = 0;
@@ -44,45 +52,10 @@ void strcpyt(char *smain, char *scopy, int t)
     }
     scopy[j] = '\0';
 }
-int strcmp(char *a, char *b)
-{
-    for(int i=0; i<4; i++)
-    {
-        if(a[i]<b[i]) return -1;
-        else if(a[i]>b[i]) return 1;
-    }
-    return 0;
-
-}
-
-
-void bubblesort(int n) //Function for implementing bubble sort
-{
-    n=n-4;
-    printf("Ran\n");
-    //)
-
-    for (int i = 0; i < n - 1; i++) //Outer Loop
-    {
-        bool flag = false;                  //to make it more efficient
-        for (int j = 0; j < n - 1 - i; j++) //inner loop
-        {
-            printf("Before: %s %s\n",sarray[j].s,sarray[j + 1].s);
-            if (strcmp(sarray[j].s,sarray[j + 1].s)==1) //Comparison
-            {                        //Swapping
-                strcpy(sarray[j].s,sarray[j + 1].s);
-                printf("After %s %s\n",sarray[j].s,sarray[j + 1].s);
-                flag = true;
-            }
-        }
-        if (!flag)
-            return;
-    }
-}
 int main()
 {
     int n = 0;
-    struct String4 sarray[100000];
+    struct String4 sarray[1000000];
     char str[100000];
     scanf("%s", str);
     int len = strlen(str);
@@ -91,9 +64,23 @@ int main()
         strcpyt(str, sarray[n].s, n);
         n++;
     }
-    bubblesort(len);
+    //Sorting The Array of Strings!
+    int i, j, count = len - 4;
+    char temp[5];
+    for (i = 0; i <= count; i++)
+        for (j = i + 1; j <= count; j++)
+        {
+            if (strcmp(sarray[i].s, sarray[j].s) > 0)
+            {
+                strcpy(temp, sarray[i].s);
+                strcpy(sarray[i].s, sarray[j].s);
+                strcpy(sarray[j].s, temp);
+            }
+        }
+
     for (int i = 0; i < len - 3; i++)
     {
         printf("%s\n", sarray[i].s);
     }
+    return 0;
 }
