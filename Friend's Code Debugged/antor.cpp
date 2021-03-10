55b8f2d056655dcef517bd9e153dc7c615255f88
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2021-03-10 22:57:02 +0600

    antor problem 2

--- Friend's Code Debugged/antor.cpp
+++ Friend's Code Debugged/antor.cpp
@@ -11,9 +11,31 @@ int hydroxide(char *s)
         return 0;
     return 1;
 }
+void reversePrint(int A[], int size)
+{
+    if (size > 1)
+    {
+        printf("%d\t", A[size - 1]);
+        reversePrint(A, size - 1);
+    }
+    if (size == 1)
+    {
+        printf("%d\t", A[0]);
+        return;
+    }
+
+    return;
+}
+
 int main()
 {
+    /*
+    Element for testing hydroxide function
     char s[10000];
     scanf("%s", s);
     printf("%d\n", hydroxide(s));
+    */
+    int A[] = {1, 2, 3, 4, 5, 6, 7};
+    //printf("%d\t" ,A[6]);
+    reversePrint(A, 7);
 }
