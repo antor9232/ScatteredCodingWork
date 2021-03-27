abbf6ce3acff4485fb620e7ad667e318907cb0a4
antor9232 <81458153+antor9232@users.noreply.github.com>
2021-03-27 11:25:50 +0600

    Update antor.cpp

--- Friend's Code Debugged/antor.cpp
+++ Friend's Code Debugged/antor.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdio.h>
 int hydroxide(char *s)
 {
     int i = 0;
@@ -11,6 +12,15 @@ int hydroxide(char *s)
         return 0;
     return 1;
 }
+int Hydroxide(char *sp)
+{ char *r;
+	char h[]="OH";
+	r=strstr(sp,h);
+	if(r)
+	return 1;
+	else return 0;
+}
+
 void reversePrint(int A[], int size)
 {
     if (size > 1)
@@ -39,12 +49,14 @@ void rpd(int number)
     return;
 }
 int main()
-{
+{   
     /*
     Element for testing hydroxide function
     char s[10000];
+    char sp[100];
     scanf("%s", s);
     printf("%d\n", hydroxide(s));
+    printf("%d\n",Hydroxide(sp));
     */
     /*
     Element for testing reversePrint function
